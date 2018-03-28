655b847907813a3792cc30f407cc355c310fbd8a
robnagler <github@q33.us>
2018-03-28 15:58:06 +0000

    ckp: acceptance and unit tests basically running

--- rsconf/component/btest.py
+++ rsconf/component/btest.py
@@ -23,7 +23,7 @@ class T(component.T):
         self.buildt.require_component('docker', 'postgresql', 'postfix')
         j2_ctx = self.hdb.j2_ctx_copy()
         z = j2_ctx.btest
-        z.run_u = j2_ctx.rsconf_db.run_u
+        z.run_u = j2_ctx.rsconf_db.root_u
         z.run_d = systemd.timer_prepare(self, j2_ctx)
         z.apps_d = z.run_d.join('apps')
         run_all = z.run_d.join('run_all')
@@ -35,22 +35,25 @@ class T(component.T):
             run_u=z.run_u,
         )
         z.source_code_d = bop.SOURCE_CODE_D
-        self.install_access(mode='700', owner=z.run_u)
+        z.app_run_u = j2_ctx.rsconf_db.run_u
+        self.install_access(mode='700', owner=z.app_run_u)
         self.install_directory(z.apps_d)
         for n in sorted(z.apps):
             a = j2_ctx[n]
             z.perl_root = a.perl_root
             self.install_access(mode='700')
-            d = z.apps_d.join(n)
-            self.install_directory(d)
+            z.app_run_d = z.apps_d.join(n)
+            self.install_directory(z.app_run_d)
+            z.docker_image = docker_registry.absolute_image(j2_ctx, a.docker_image)
+            z.container_name = 'btest_' + n
             z.current_base = 'current'
-            z.current_d = d.join(z.current_base)
-            z.file_root = d.join('db')
+            z.current_d = z.app_run_d.join(z.current_base)
+            z.file_root = z.app_run_d.join('db')
             self.install_directory(z.file_root)
-            z.backup_root = d.join('bkp')
+            z.backup_root = z.app_run_d.join('bkp')
             self.install_directory(z.backup_root)
             z.database = a.short_prefix + 'btest'
-            z.log_d = d.join('log')
+            z.log_d = z.app_run_d.join('log')
             # handles petshop
             z.root_for_unit = re.sub('::.*', '', z.perl_root)
             z.app_source_d = pkio.py_path('/usr/src/bop').join(z.root_for_unit)
@@ -62,20 +65,27 @@ class T(component.T):
             z.http_home_page_uri = 'https://' + a.http_host
             z.http_host = a.http_host
             z.mail_host = a.mail_host
-            z.email_user = z.run_u
+            z.email_user = z.app_run_u
             self.install_access(mode='400')
             z.bconf = pkcollections.Dict()
             for x in 'unit', 'acceptance':
                 z.bconf_is_unit = x == 'unit'
-                z.bconf[x] = d.join('bivio-{}.bconf'.format(x))
+                z.bconf[x] = z.app_run_d.join('bivio-{}.bconf'.format(x))
                 self.install_resource(
                     'btest/bivio.bconf',
                     j2_ctx,
                     z.bconf[x],
                 )
             self.install_access(mode='500')
+            z.app_run_f = z.app_run_d.join('run')
             self.install_resource(
-                'btest/run_one.sh',
+                'btest/app_run.sh',
                 j2_ctx,
-                d.join('run_one'),
+                z.app_run_f,
+            )
+            z.app_start_f = z.app_run_d.join('start')
+            self.install_resource(
+                'btest/app_start.sh',
+                j2_ctx,
+                z.app_start_f,
             )
