a6737916e5394b068771ca12f367ef0664f8e0b0
robnagler <github@q33.us>
2018-05-14 21:36:23 +0000

    added network.untrusted for Linode

--- rsconf/component/network.py
+++ rsconf/component/network.py
@@ -30,9 +30,6 @@ def update_j2_ctx(j2_ctx):
 class T(component.T):
 
     def internal_build(self):
-        #TODO(robnagler) need to handle dfw1 - dhcp (no net config) and trusted single ip
-        # not all nets are trusted (dfw3) so we need trusted and untrusted nets.
-        # for now works fine.
         self.buildt.require_component('base_os')
         if not self.hdb.network.devices:
             # no devices, no network config
@@ -42,7 +39,7 @@ class T(component.T):
         j2_ctx = self.hdb.j2_ctx_copy()
         update_j2_ctx(j2_ctx)
         self.install_access(mode='444', owner=self.hdb.rsconf_db.root_u)
-        devs, defroute = _devices(self, j2_ctx)
+        devs, defroute = self._devices(j2_ctx)
         if defroute:
             assert defroute.net.search and defroute.net.nameservers, \
                 '{}: defroute needs search and nameservers'.format(defroute.net)
@@ -70,6 +67,89 @@ class T(component.T):
             self.hdb.network.primary_public_ip = z.inet_dev.ip
         self._write_files(j2_ctx, devs)
 
+    def _defroute(self, routes):
+        defroute = None
+        for r in sorted(routes, key=lambda x: x.name):
+            if not defroute:
+                defroute = r
+                continue
+            dig = defroute.net.name.is_global
+            rig = r.net.name.is_global
+            if dig == rig:
+                assert not dig, \
+                    '{} & {}: are both global routes'.format(defroute.name, r.name)
+                # unlikely case: defaults to the lowest non-global network route
+            if rig:
+                defroute = r
+        return defroute
+
+    def _devices(self, j2_ctx):
+        z = j2_ctx.network
+        self.__trusted_nets = self._nets(j2_ctx, z.trusted)
+        j2_ctx.network.setdefault(
+            'trusted_public_nets',
+            sorted([n.name for n in self.__trusted_nets.values() if n.name.is_global]),
+        )
+        self.__untrusted_nets = self._nets(j2_ctx, z.trusted)
+        devs = []
+        routes = []
+        defroute = None
+        j2_ctx.network.natted_dev = None
+        for dn in sorted(j2_ctx.network.devices):
+            d = copy.deepcopy(j2_ctx.network.devices[dn])
+            devs.append(d)
+            d.name = dn
+            d.vlan = '.' in dn
+            d.net = self._net_check(d.ip)
+            if d.net.gateway:
+                routes.append(d)
+            if d.setdefault('defroute', False):
+                assert not defroute, \
+                    '{} & {}: both declared as default routes'.format(d, defroute)
+                defroute = d
+            if d.setdefault('is_natted', False):
+                assert not j2_ctx.network.natted_dev, \
+                    '{}: duplicate natted dev ({})'.format(
+                        d.name,
+                        j2_ctx.network.natted_dev.name,
+                    )
+                j2_ctx.network.natted_dev = d
+        if not defroute:
+            defroute = self._defroute(routes)
+        defroute.defroute = True
+        j2_ctx.network.defroute = defroute
+        return devs, defroute
+
+    def _nets(self, j2_ctx, spec):
+        nets = pkcollections.Dict()
+        for n, v in spec.items():
+            v = copy.deepcopy(v)
+            # Avoid this error:
+            # Did you pass in a bytes (str in Python 2) instead of a unicode object?
+            n = ipaddress.ip_network(pkcompat.locale_str(n))
+            v.name = n
+            v.ip = str(n.network_address)
+            v.netmask = str(n.netmask)
+            g = v.setdefault('gateway', '')
+            if g:
+                assert ipaddress.ip_network(pkcompat.locale_str(g + '/32')).subnet_of(n), \
+                    '{}: gateway is not subnet of {}'.format(g, n)
+            if 'nameservers' in v:
+                v.nameservers = sorted([ipaddress.ip_address(x) for x in v.nameservers])
+            nets[n] = v
+        return nets
+
+    #TODO(robnagler) sanity check nets don't overlap?
+    def _net_check(self, ip):
+        nip = ipaddress.ip_network(ip + '/32')
+        #TODO(robnagler) untrusted networks are supernets of potentially trusted_nets
+        # the only reason to add them is to find them for devices
+        for nets in self.__untrusted_nets, self.__trusted_nets:
+            for n in nets:
+                if nip.subnet_of(n):
+                    return nets[n]
+        raise AssertionError('{}: ip not found in network.trusted'.format(nip))
+
     def _write_files(self, j2_ctx, devs):
         # Only for jupyterhub, explicitly set, and not on a machine
         # with a public address
@@ -90,85 +170,3 @@ class T(component.T):
                 )
             self.install_resource('network/iptables', j2_ctx, _IPTABLES)
         self.append_root_bash_with_main(j2_ctx)
-
-
-def _defroute(routes):
-    defroute = None
-    for r in sorted(routes, key=lambda x: x.name):
-        if not defroute:
-            defroute = r
-            continue
-        dig = defroute.net.name.is_global
-        rig = r.net.name.is_global
-        if dig == rig:
-            assert not dig, \
-                '{} & {}: are both global routes'.format(defroute.name, r.name)
-            # unlikely case: defaults to the lowest non-global network route
-        if rig:
-            defroute = r
-    return defroute
-
-
-def _devices(self, j2_ctx):
-    nets, net_check = _nets(j2_ctx)
-    devs = []
-    routes = []
-    defroute = None
-    j2_ctx.network.natted_dev = None
-    for dn in sorted(j2_ctx.network.devices):
-        d = copy.deepcopy(j2_ctx.network.devices[dn])
-        devs.append(d)
-        d.name = dn
-        d.vlan = '.' in dn
-        d.net = net_check(d.ip)
-        if d.net.gateway:
-            routes.append(d)
-        if d.setdefault('defroute', False):
-            assert not defroute, \
-                '{} & {}: both declared as default routes'.format(d, defroute)
-            defroute = d
-        if d.setdefault('is_natted', False):
-            assert not j2_ctx.network.natted_dev, \
-                '{}: duplicate natted dev ({})'.format(
-                    d.name,
-                    j2_ctx.network.natted_dev.name,
-                )
-            j2_ctx.network.natted_dev = d
-    if not defroute:
-        defroute = _defroute(routes)
-    defroute.defroute = True
-    j2_ctx.network.defroute = defroute
-    return devs, defroute
-
-
-def _nets(j2_ctx):
-    nets = pkcollections.Dict()
-
-    #TODO(robnagler) sanity check nets don't overlap?
-    def net_check(ip):
-        nip = ipaddress.ip_network(ip + '/32')
-        for n in nets:
-            if nip.subnet_of(n):
-                return nets[n]
-        raise AssertionError('{}: ip not found in network.trusted'.format(nip))
-
-    for n, v in j2_ctx.network.trusted.items():
-        v = copy.deepcopy(v)
-        # Avoid this error:
-        # Did you pass in a bytes (str in Python 2) instead of a unicode object?
-        n = ipaddress.ip_network(pkcompat.locale_str(n))
-        v.name = n
-        v.ip = str(n.network_address)
-        v.netmask = str(n.netmask)
-        g = v.setdefault('gateway', '')
-        if g:
-            assert ipaddress.ip_network(pkcompat.locale_str(g + '/32')).subnet_of(n), \
-                '{}: gateway is not subnet of {}'.format(g, n)
-        if 'nameservers' in v:
-            v.nameservers = sorted([ipaddress.ip_address(x) for x in v.nameservers])
-        nets[n] = v
-    j2_ctx.network.setdefault(
-        'trusted_public_nets',
-        sorted([n.name for n in nets.values() if n.name.is_global]),
-    )
-    return nets, net_check
