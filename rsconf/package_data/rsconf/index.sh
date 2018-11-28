ce6b02a90be336fe1d4de74e0d095a8a31cdea2e
robnagler <github@q33.us>
2018-11-28 21:48:15 +0000

    rsconf.radiasoft.org is the default install_server

--- rsconf/package_data/rsconf/index.sh
+++ rsconf/package_data/rsconf/index.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 set -eu -o pipefail
+if [[ ! ${install_server:-} ]]; then
+    export install_server=https://rsconf.radiasoft.org
+fi
 curl -s -S -L "$install_server/radiasoft/download/bin/install.sh" | bash -s rsconf.sh "$@"
