c9976d8e0cb3f880f28e2a6094800f0236cde83c
robnagler <github@q33.us>
2019-03-14 11:21:08 +0000

    ckp: added rsdockerspawner_cfg

--- rsconf/component/jupyterhub.py
+++ rsconf/component/jupyterhub.py
@@ -6,6 +6,7 @@ u"""create sirepo configuration
 """
 from __future__ import absolute_import, division, print_function
 from pykern import pkcollections
+from pykern import pkjson
 from rsconf import component
 
 
@@ -13,6 +14,10 @@ _CONF_F = 'conf.py'
 _COOKIE_SECRET = 'jupyterhub_cookie_secret'
 _PROXY_AUTH = 'jupyterhub_proxy_auth'
 _USER_SUBDIR = 'user'
+_DOCKER_TLS_SUBDIR = 'docker_tls'
+_DEFAULT_PORT_BASE = 8800
+# POSIT: rsdockerspawner._DEFAULT_POOL_NAME
+_DEFAULT_POOL_NAME = 'default'
 
 
 class T(component.T):
@@ -26,9 +31,9 @@ class T(component.T):
         j2_ctx = self.hdb.j2_ctx_copy()
         z = j2_ctx.jupyterhub
         z.vhost = j2_ctx.jupyterhub.vhosts[j2_ctx.rsconf_db.host]
-        run_d = systemd.docker_unit_prepare(self, j2_ctx)
+        z.run_d = systemd.docker_unit_prepare(self, j2_ctx)
         z.update(
-            user_d=run_d.join(_USER_SUBDIR),
+            user_d=z.run_d.join(_USER_SUBDIR),
             jupyter_docker_image=docker_registry.absolute_image(
                 j2_ctx, z.jupyter_docker_image,
             ),
@@ -37,7 +42,7 @@ class T(component.T):
         )
         z.home_d = db.user_home_path(j2_ctx, z.jupyter_run_u)
         self.install_access(mode='711', owner=z.run_u)
-        self.install_directory(run_d)
+        self.install_directory(z.run_d)
         self.install_access(mode='700', owner=z.jupyter_run_u)
         self.install_directory(z.user_d)
         self.install_access(mode='400', owner=z.run_u)
@@ -55,7 +60,9 @@ class T(component.T):
         if z.get('whitelist_users'):
             # admin_users are implicitly part of whitelist
             z.whitelist_users_str = _list_to_str(z.whitelist_users)
-        conf_f = run_d.join(_CONF_F)
+        if z.get('pools'):
+            self._rsdockerspawner(j2_ctx, z)
+        conf_f = z.run_d.join(_CONF_F)
         self.install_resource('jupyterhub/{}'.format(_CONF_F), j2_ctx, conf_f)
         self.append_root_bash(
             "rsconf_service_docker_pull '{}'".format(z.jupyter_docker_image),
@@ -69,6 +76,41 @@ class T(component.T):
             run_u=z.run_u,
             volumes=[docker.DOCKER_SOCK],
         )
+
+    def _rsdockerspawner(self, j2_ctx, z):
+        c = pkcollections.Dict(
+            port_base=z.setdefault('port_base', _DEFAULT_PORT_BASE),
+            tls_dir=z.run_dir(_DOCKER_TLS_SUBDIR),
+        )
+        seen = pkcollections.Dict(
+            hosts=pkcollections.Dict(),
+            users=pkcollections.Dict(),
+        )
+        for n, p in z.pools.items():
+            p.setdefault('users', [])
+            assert p.users or n == _DEFAULT_POOL_NAME, \
+                'no users in pool={}'.format(n)
+            assert p.setdefault('servers_per_host', 0) > 1, \
+                'invalid servers_per_host={} in pool={}'.format(
+                    p.servers_per_host,
+                    n,
+                )
+            for x in 'hosts', 'users':
+                for y in p[x]:
+                    assert y not in seen[x], \
+                        'duplicate value={} in {} pool={}'.format(y, x, n)
+                    seen[x][y] = n
+            p.setdefault('mem_limit', None)
+            p.setdefault('cpu_limit', None)
+        c.pools = z.pools
+        docker.setup_cluster(
+            self,
+            hosts=seen.hosts.keys(),
+            tls_d=z.docker_tls_d,
+            run_u=z.run_u,
+            j2_ctx=j2_ctx,
+        )
+        z.rsdockerspawner_cfg = pkjson.dump_pretty(c)
 
 
 def _list_to_str(v):
