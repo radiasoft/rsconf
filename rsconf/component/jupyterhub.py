18e922a3a6e7018158157280c22cd33b5288d8df
robnagler <bitbucket@q33.us>
2019-08-20 14:59:13 +0000

    support jupyterhub user_groups

--- rsconf/component/jupyterhub.py
+++ rsconf/component/jupyterhub.py
@@ -5,6 +5,7 @@ u"""create sirepo configuration
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
 from __future__ import absolute_import, division, print_function
+from pykern.pkdebug import pkdp
 from pykern import pkcollections
 from pykern import pkjson
 from rsconf import component
@@ -17,7 +18,9 @@ _USER_SUBDIR = 'user'
 _DOCKER_TLS_SUBDIR = 'docker_tls'
 _DEFAULT_PORT_BASE = 8100
 # POSIT: rsdockerspawner._DEFAULT_POOL_NAME
-_DEFAULT_POOL_NAME = 'default'
+_DEFAULT_POOL_V2 = 'default'
+_DEFAULT_USER_GROUP = 'everybody'
+_DEFAULT_POOL_V3 = _DEFAULT_USER_GROUP
 _DEFAULT_MOCK_PASSWORD = 'testpass'
 
 
@@ -59,6 +62,7 @@ class T(component.T):
             visibility='channel',
         )[0]
         z.admin_users_str = _list_to_str(z.admin_users)
+        # deprecated but does not conflict so leave
         if z.get('whitelist_users'):
             # admin_users are implicitly part of whitelist
             z.whitelist_users_str = _list_to_str(z.whitelist_users)
@@ -100,9 +104,25 @@ class T(component.T):
             hosts=pkcollections.Dict(),
             users=pkcollections.Dict(),
         )
+        if z.get('user_groups'):
+            self._rsdockerspawner_v3(j2_ctx, z, seen)
+            c.user_groups = z.user_groups
+            c.volumes = z.volumes
+        else:
+            self._rsdockerspawner_v2_deprecated(j2_ctx, z, seen)
+        docker.setup_cluster(
+            self,
+            hosts=seen.hosts.keys(),
+            tls_d=tls_d,
+            run_u=z.run_u,
+            j2_ctx=j2_ctx,
+        )
+        c.pools = z.pools
+        z.rsdockerspawner_cfg = pkjson.dump_pretty(c)
+
+    def _rsdockerspawner_v2_deprecated(self, j2_ctx, z, seen):
         for n, p in z.pools.items():
-            p.setdefault('users', [])
-            assert p.users or n == _DEFAULT_POOL_NAME, \
+            assert p.users or n == _DEFAULT_POOL_V2, \
                 'no users in pool={}'.format(n)
             assert p.setdefault('servers_per_host', 0) >= 1, \
                 'invalid servers_per_host={} in pool={}'.format(
@@ -117,15 +137,57 @@ class T(component.T):
             p.setdefault('mem_limit', None)
             p.setdefault('cpu_limit', None)
             p.setdefault('min_activity_hours', None)
-        c.pools = z.pools
-        docker.setup_cluster(
-            self,
-            hosts=seen.hosts.keys(),
-            tls_d=tls_d,
-            run_u=z.run_u,
-            j2_ctx=j2_ctx,
+
+    def _rsdockerspawner_v3(self, j2_ctx, z, seen):
+        def _users_for_groups(groups, t, n):
+            res = set()
+            for g in groups:
+                if g == _DEFAULT_USER_GROUP:
+                    return []
+                u = z.user_groups.get(g)
+                assert u is not None, \
+                    'user_group={} not found for {}={}'.format(g ,t, n)
+                res.union(u)
+            return sorted(res)
+
+        # POSIT: notebook_dir in
+        # radiasoft/container-beamsim-jupyter/container-conf/build.sh
+        # parameterize anyway, because matches above
+        z.volumes.setdefault(
+            str(z.user_d.join('{username}')),
+            pkcollections.Dict(bind=str(z.home_d.join('jupyter'))),
         )
-        z.rsdockerspawner_cfg = pkjson.dump_pretty(c)
+        for n, v in z.volumes.items():
+            m = v.get('mode')
+            if not isinstance(m, dict):
+                continue
+            seen2 = set()
+            for g in m.values():
+                for u in _users_for_groups(g, 'volume', n):
+                    assert u not in seen2, \
+                        'user={} in both modes for volume={}'.format(u, n)
+                    seen2.add(u)
+        for n, p in z.pools.items():
+            for h in p['hosts']:
+                assert h not in seen.hosts, \
+                    'duplicate host={} in two pools {} and {}'.format(
+                        h,
+                        n,
+                        seen.hosts[h],
+                    )
+                seen.hosts[h] = n
+            if n == _DEFAULT_POOL_V3:
+                assert 'user_groups' not in p, \
+                    'user_groups may not be specifed for pool={}'.format(n)
+            else:
+                for u in _users_for_groups(p.user_groups, 'pool', n):
+                    assert s not in seen.users, \
+                        'duplicate user={} in two pools {} and {}'.format(
+                            s,
+                            n,
+                            seen.hosts[s],
+                        )
+                    seen.users[s] = n
 
 
 def _list_to_str(v):
