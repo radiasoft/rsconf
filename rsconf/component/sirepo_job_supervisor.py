7205d0d2270f400c126659306cb6b1f7edfae4c2
robnagler <github@q33.us>
2020-01-05 10:27:24 +0000

    shifter_image needs to not include docker.io registry

--- rsconf/component/sirepo_job_supervisor.py
+++ rsconf/component/sirepo_job_supervisor.py
@@ -86,5 +86,5 @@ class T(component.T):
 
     def _module_sbatch(self, jc):
         self.j2_ctx_pksetdefault({
-            'sirepo.job_driver.sbatch.shifter_image': jc.sirepo.docker_image,
+            'sirepo.job_driver.sbatch.shifter_image': 'radiasoft/sirepo:' + jc.rsconf_db.channel,
         })
