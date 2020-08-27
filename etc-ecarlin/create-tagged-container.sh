4f0ff8f171a5e62baaf32f9b34728a16a7490382
e-carlin <evan@carlin.com>
2020-08-27 20:11:47 +0000

    ckp

--- etc-ecarlin/create-tagged-container.sh
+++ etc-ecarlin/create-tagged-container.sh
@@ -1,4 +1,6 @@
+set -eou pipefail
 name="$1"
+docker pull "docker.io/radiasoft/$name"
 docker tag "docker.io/radiasoft/$name" "v3.radia.run:5000/radiasoft/$name:dev"
 sudo cat /root/.docker/config.json > ~/.docker/config.json
 docker push "v3.radia.run:5000/radiasoft/$name:dev"
