0ac44d0d53ae37dad34c90c8c81cb94400b911fd
robnagler <github@q33.us>
2022-10-03 08:18:11 +0000

    update test data

--- tests/pkcli/build_data/1.out/srv/host/v9.radia.run/bkp.sh
+++ tests/pkcli/build_data/1.out/srv/host/v9.radia.run/bkp.sh
@@ -2,7 +2,7 @@
 bkp_rsconf_component() {
 rsconf_service_prepare 'bkp.timer' '/etc/systemd/system/bkp.service' '/etc/systemd/system/bkp.timer' '/srv/bkp'
 rsconf_install_access '500' 'root' 'root'
-rsconf_install_file '/srv/bkp/secondary' 'b378d96f4acf76465a22b7f1719645bc'
+rsconf_install_file '/srv/bkp/secondary' 'c18b548ce38fe67c256d8437889662a4'
 rsconf_install_file '/srv/bkp/secondary_setup' '84b40460f8374ccbde4c353cc301f5fc'
 rsconf_install_access '700' 'root' 'root'
 rsconf_install_directory '/srv/bkp'
