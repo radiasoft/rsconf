8613e9a80a2b8c9a448f97e16f65f6fb65a8b89d
e-carlin <evan@carlin.com>
2022-10-21 15:27:58 +0000

    resolve pr comments

--- rsconf/component/raydata_scan_monitor.py
+++ rsconf/component/raydata_scan_monitor.py
@@ -9,19 +9,22 @@ from pykern.pkcollections import PKDict
 from pykern.pkdebug import pkdp
 from rsconf import component
 from rsconf import systemd
+import rsconf.db
 
 _DB_SUBDIR = "db"
+
+_INTAKE_D = "intake"
 
 
 class T(component.T):
     def internal_build_compile(self):
         self.buildt.require_component("docker")
         jc, z = self.j2_ctx_init()
+        z.run_u = jc.rsconf_db.run_u
         z.db_d = systemd.docker_unit_prepare(self, jc).join(_DB_SUBDIR)
-        z.intake_d = jc.systemd.run_d.join("intake")
+        z.intake_d = jc.systemd.run_d.join(_INTAKE_D)
 
     def internal_build_write(self):
-
         jc = self.j2_ctx
         z = jc[self.name]
         systemd.docker_unit_enable(
@@ -34,20 +37,17 @@ class T(component.T):
                 z.db_d,
                 [
                     z.intake_d,
-                    # POSIT: python -c "import databroker; print(databroker.catalog_search_path())"
-                    f"/home/{jc.rsconf_db.run_u}/.local/share/intake",
+                    # POSIT: databroker.catalog_search_path()
+                    f"{rsconf.db.user_home_path(jc, z.run_u)}/.local/share/intake",
                     "rw",
                 ],
             ],
         )
-        self.install_access(mode="700", owner=jc.rsconf_db.run_u)
+        self.install_access(mode="700", owner=z.run_u)
         self.install_directory(z.db_d)
         self.install_directory(z.intake_d)
 
     def _unit_env(self, z):
         return pkconfig.to_environ(
-            ["raydata.pkcli.*"],
-            values=z.pkmerge(
-                PKDict(raydata=PKDict(pkcli=PKDict(scan_monitor=PKDict(db_dir=z.db_d))))
-            ),
+            ["raydata.pkcli.*"], values={"raydata.pkcli.scan_monitor.db_dir": z.db_d}
         )
