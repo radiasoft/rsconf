7c8939114e082d3ad19bfd9bd4bf38042ff564a3
Rob Nagler <github@q33.us>
2023-02-15 15:24:53 -0700

    Fix #289 simple script component (#291)

---
+++ rsconf/component/script.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""run arbitrary scripts with dependencies
+
+:copyright: Copyright (c) 2023 RadiaSoft LLC.  All Rights Reserved.
+:license: http://www.apache.org/licenses/LICENSE-2.0.html
+"""
+from pykern.pkdebug import pkdp
+from rsconf import component
+
+
+class T(component.T):
+    def internal_build_compile(self):
+        _, z = self.j2_ctx_init()
+        self.buildt.require_component(*(z.get("require_components", ["base_users"])))
+
+    def internal_build_write(self):
+        self.append_root_bash(self.j2_ctx.script.bash)
---
+++ tests/pkcli/build_data/1.out/srv/host/v4.radia.run/script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+script_rsconf_component() {
+echo hello
+
+}
