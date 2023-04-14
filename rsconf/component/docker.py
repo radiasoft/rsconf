034960924145a7bc4a400fa1e4f69894aef2348d
e-carlin <evan@carlin.com>
2023-04-14 22:08:49 +0000

    pr comments

--- rsconf/component/docker.py
+++ rsconf/component/docker.py
@@ -32,7 +32,7 @@ class T(component.T):
         self.buildt.require_component("base_all", "db_bkp")
         j2_ctx = self.hdb.j2_ctx_copy()
         z = j2_ctx.docker
-        self._check_min_version(z)
+        self._check_min_software_version(z)
         systemd.unit_prepare(
             self,
             j2_ctx,
@@ -79,12 +79,13 @@ class T(component.T):
             run_d=z.run_d,
         )
 
-    def _check_min_version(self, z):
-        z.setdefault("min_version", "0.0.0")
-        p = [int(v) for v in z.min_version.split(".")]
+    def _check_min_software_version(self, z):
+        z.setdefault("min_software_version", "0.0.0")
+        p = [int(v) for v in z.min_software_version.split(".")]
         if len(p) != 3:
-            raise ValueError(f"min_version={z.min_version} needs to be in X.X.X format")
-        z.min_version = p
+            raise ValueError(
+                f"min_software_version={z.min_software_version} needs to be in X.X.X format"
+            )
 
     def _install_daemon_json(self, z):
         p = self.tmp_path()
