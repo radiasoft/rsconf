859c58a2318bfdbc6665d31bf23eb48409b64462
Rob Nagler <github@q33.us>
2023-05-15 18:21:48 -0600

    Revert "Fix #322 Fix cipher names; use Cloudflare modern, ssl labs strong (#324)" (#325)
    This reverts commit 797a11cd1e6c0d67cebb250e80103e8036019ecc.

--- tests/pkcli/build_data/1.out/srv/host/v3.radia.run/nginx.sh
+++ tests/pkcli/build_data/1.out/srv/host/v3.radia.run/nginx.sh
@@ -6,7 +6,7 @@ rsconf_service_prepare 'nginx' '/etc/systemd/system/nginx.service' '/etc/systemd
 rsconf_install_access '400' 'root' 'root'
 rsconf_install_file '/etc/nginx/conf.d/v3.radia.run.key' 'b98bb61f3b76969ee1a259d5a1afd5f1'
 rsconf_install_file '/etc/nginx/conf.d/v3.radia.run.crt' '71151618f3ca16bad2d73ef5a6683c1c'
-rsconf_install_file '/etc/nginx/nginx.conf' '9d4be2df071b8a3649e3f94ca884220a'
+rsconf_install_file '/etc/nginx/nginx.conf' 'f179885925d32e655b98829c9d66f32c'
 rsconf_install_access '755' 'root' 'root'
 rsconf_install_directory '/srv/www'
 rsconf_service_restart_at_end 'nginx'
