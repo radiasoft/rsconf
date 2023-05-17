8b8935ba9de672a26e79707f82ac2bb696bb92dd
Justin Schell <jschell@bivio.biz>
2023-05-17 21:55:49 +0000

    Fix #322 use Cloudflare modern for PCI DSS, ssl labs strong

--- tests/pkcli/build_data/1.out/srv/host/v3.radia.run/nginx.sh
+++ tests/pkcli/build_data/1.out/srv/host/v3.radia.run/nginx.sh
@@ -6,7 +6,7 @@ rsconf_service_prepare 'nginx' '/etc/systemd/system/nginx.service' '/etc/systemd
 rsconf_install_access '400' 'root' 'root'
 rsconf_install_file '/etc/nginx/conf.d/v3.radia.run.key' 'b98bb61f3b76969ee1a259d5a1afd5f1'
 rsconf_install_file '/etc/nginx/conf.d/v3.radia.run.crt' '71151618f3ca16bad2d73ef5a6683c1c'
-rsconf_install_file '/etc/nginx/nginx.conf' 'f179885925d32e655b98829c9d66f32c'
+rsconf_install_file '/etc/nginx/nginx.conf' '4d818eb80f882af9ebc7aa39be02bde7'
 rsconf_install_access '755' 'root' 'root'
 rsconf_install_directory '/srv/www'
 rsconf_service_restart_at_end 'nginx'
