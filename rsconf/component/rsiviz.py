6ff4b6a69e4bed2f0fba4b295f50776449b20ed7
e-carlin <evan@carlin.com>
2023-11-09 21:19:43 +0000

    Fix #407: More deployment fixes for viz3d sirepo app
    - Nginx needs to listen on public host (currently listens on backnet)
    - Add trailing / in nginx config so routes are proxied through
    - Add VIZ3D to index_fmt
    - Add all public ports that IndeX could be assigned to the list of allowed_origins

--- rsconf/component/rsiviz.py
+++ rsconf/component/rsiviz.py
@@ -1,25 +1,43 @@
 # -*- coding: utf-8 -*-
 """rsiviz (nginx + IndeX)
 
-:copyright: Copyright (c) 2019 RadiaSoft LLC.  All Rights Reserved.
+:copyright: Copyright (c) 2019-2023 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
+from pykern import pkconfig
 from pykern.pkcollections import PKDict
 from pykern.pkdebug import pkdp
 from rsconf import component
 from rsconf import db
-from rsconf import systemd
+import socket
 
 
 class T(component.T):
     def internal_build_compile(self):
         self.buildt.require_component("nginx")
         jc, z = self.j2_ctx_init()
-        self.__host = self.hdb.rsconf_db.host
+
+        n = self.buildt.get_component("network")
+        if pkconfig.in_dev_mode():
+            h = jc.rsconf_db.host
+        else:
+            i = n.unchecked_public_ip()
+            assert i, "must have a public ip outside of dev"
+            h = socket.gethostbyaddr(i)[0]
+        self.__host = h
         if "index_uri_secret" not in z:
             z.index_uri_secret = db.random_string()
-        z.global_resources.index_uri_fmt = (
+        z.global_resources.viz3d.index_uri_fmt = (
             f"https://{self.__host}:{{}}/{z.index_uri_secret}/"
+        )
+        z.global_resources.viz3d.index_allowed_origins = f" ".join(
+            [
+                f"{self.__host}:{p}"
+                for p in range(
+                    jc.sirepo.global_resources.public_ports_min,
+                    jc.sirepo.global_resources.public_ports_max,
+                )
+            ]
         )
 
     def internal_build_write(self):
@@ -32,6 +50,6 @@ class T(component.T):
         )
 
     def sirepo_config(self, sirepo):
-        sirepo.j2_ctx.rsiviz.global_resources.index_uri_fmt = (
-            self.j2_ctx.rsiviz.global_resources.index_uri_fmt
+        sirepo.j2_ctx.rsiviz.global_resources.viz3d = (
+            self.j2_ctx.rsiviz.global_resources.viz3d
         )
