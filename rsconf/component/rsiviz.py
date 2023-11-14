ab8e26caa85f15729cbd7b48e39edd3eb9b3b3f5
Evan Carlin <evan@carlin.com>
2023-11-14 12:32:11 -0700

    Fix #407: More deployment fixes for viz3d sirepo app (#408)
    - Nginx needs to listen on public host (currently listens on backnet)
    - Add trailing / in nginx config so routes are proxied through
    - Add VIZ3D to index_fmt
    - Add all public ports that IndeX could be assigned to the list of allowed_origins

--- rsconf/component/rsiviz.py
+++ rsconf/component/rsiviz.py
@@ -1,25 +1,31 @@
 # -*- coding: utf-8 -*-
 """rsiviz (nginx + IndeX)
 
-:copyright: Copyright (c) 2019 RadiaSoft LLC.  All Rights Reserved.
+:copyright: Copyright (c) 2019-2023 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
+from pykern import pkconfig
 from pykern.pkcollections import PKDict
 from pykern.pkdebug import pkdp
 from rsconf import component
 from rsconf import db
-from rsconf import systemd
 
 
 class T(component.T):
     def internal_build_compile(self):
         self.buildt.require_component("nginx")
         jc, z = self.j2_ctx_init()
-        self.__host = self.hdb.rsconf_db.host
-        if "index_uri_secret" not in z:
-            z.index_uri_secret = db.random_string()
-        z.global_resources.index_uri_fmt = (
-            f"https://{self.__host}:{{}}/{z.index_uri_secret}/"
+        z.global_resources.viz3d.index_uri_fmt = (
+            f"https://{z.index_vhost}:{{}}/{z.index_uri_secret}/"
+        )
+        z.global_resources.viz3d.index_allowed_origins = f" ".join(
+            [
+                f"{z.index_vhost}:{p}"
+                for p in range(
+                    jc.sirepo.global_resources.public_ports_min,
+                    jc.sirepo.global_resources.public_ports_max,
+                )
+            ]
         )
 
     def internal_build_write(self):
@@ -27,11 +33,11 @@ class T(component.T):
 
         nginx.install_vhost(
             self,
-            vhost=self.__host,
+            vhost=self.j2_ctx.rsiviz.index_vhost,
             j2_ctx=self.j2_ctx,
         )
 
     def sirepo_config(self, sirepo):
-        sirepo.j2_ctx.rsiviz.global_resources.index_uri_fmt = (
-            self.j2_ctx.rsiviz.global_resources.index_uri_fmt
+        sirepo.j2_ctx.rsiviz.global_resources.viz3d = (
+            self.j2_ctx.rsiviz.global_resources.viz3d
         )
