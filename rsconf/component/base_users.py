47191866d336d1dd4e4aee37593f60980f1995a3
robnagler <github@q33.us>
2023-11-27 21:45:09 +0000

    Fix #416 added public_ssh_key for a user

--- rsconf/component/base_users.py
+++ rsconf/component/base_users.py
@@ -4,15 +4,15 @@
 :copyright: Copyright (c) 2017 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
-from __future__ import absolute_import, division, print_function
-from rsconf import component
 from pykern.pkcollections import PKDict
+from pykern.pkdebug import pkdp
+from rsconf import component
 import copy
 
 
 class T(component.T):
     def internal_build_compile(self):
-        self.buildt.require_component("base_os")
+        self.buildt.require_component("base_os", "network")
         jc = self.j2_ctx = self.hdb.j2_ctx_copy()
         z = jc.base_users
         z.add_cmds = ""
@@ -43,6 +43,7 @@ class T(component.T):
                 )
                 else i.email
             )
+            self._ssh_key(i)
             z.added[u] = i
 
     def internal_build_write(self):
@@ -57,6 +58,13 @@ class T(component.T):
         )
         bkp.append_authorized_key(self, jc)
         self.append_root_bash_with_main(jc)
+
+    def _ssh_key(self, user):
+        if not self.buildt.get_component("network").j2_ctx.pkunchecked_nested_get(
+            "network.public_ssh_ports"
+        ) or not (k := user.get("public_ssh_key")):
+            return
+        self.append_root_bash(f"rsconf_append_authorized_key '{user.name}' '{k}'")
 
     def user_spec(self, name):
         return copy.deepcopy(self.j2_ctx.base_users.added[name])
