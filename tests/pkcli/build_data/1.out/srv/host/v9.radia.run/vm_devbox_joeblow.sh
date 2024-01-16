2fc311d5034aa6c118986aafa8c9c9cae65e8a7a
e-carlin <evan@carlin.com>
2024-01-16 17:18:39 +0000

    fix tests

--- tests/pkcli/build_data/1.out/srv/host/v9.radia.run/vm_devbox_joeblow.sh
+++ tests/pkcli/build_data/1.out/srv/host/v9.radia.run/vm_devbox_joeblow.sh
@@ -4,7 +4,7 @@ rsconf_service_prepare 'vm_devbox_joeblow' '/etc/systemd/system/vm_devbox_joeblo
 rsconf_install_access '700' 'vagrant' 'vagrant'
 rsconf_install_directory '/srv/vm_devbox_joeblow/v'
 rsconf_install_access '500' 'vagrant' 'vagrant'
-rsconf_install_file '/srv/vm_devbox_joeblow/start' 'e7f1bf9a7c05ed3fdfcf593137bc6d47'
+rsconf_install_file '/srv/vm_devbox_joeblow/start' '839c6fd2c437d24d4cae44dcd243db26'
 rsconf_install_access '444' 'root' 'root'
 rsconf_install_file '/etc/systemd/system/vm_devbox_joeblow.service' '0281d04b4abaec88ab85a6751aa3587f'
 }
