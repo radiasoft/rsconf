1ea9c85c694f1472359a801d5e00e87dc35fa9f6
e-carlin <evan@carlin.com>
2024-01-16 23:14:04 +0000

    use python script

--- tests/pkcli/build_data/1.out/srv/host/v9.radia.run/vm_devbox_joeblow.sh
+++ tests/pkcli/build_data/1.out/srv/host/v9.radia.run/vm_devbox_joeblow.sh
@@ -2,7 +2,7 @@
 vm_devbox_joeblow_rsconf_component() {
 rsconf_service_prepare 'vm_devbox_joeblow' '/etc/systemd/system/vm_devbox_joeblow.service' '/etc/systemd/system/vm_devbox_joeblow.service.d'
 rsconf_install_access '500' 'vagrant' 'vagrant'
-rsconf_install_file '/srv/vm_devbox_joeblow/start' 'b35680d49c21c91b4cdd81fa78086357'
+rsconf_install_file '/srv/vm_devbox_joeblow/start' '48c7cf09c9e25cd6112afe16cce3d4bd'
 rsconf_install_access '444' 'root' 'root'
 rsconf_install_file '/etc/systemd/system/vm_devbox_joeblow.service' '0281d04b4abaec88ab85a6751aa3587f'
 }
