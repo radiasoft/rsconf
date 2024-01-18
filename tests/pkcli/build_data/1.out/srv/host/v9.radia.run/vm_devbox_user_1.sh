d3bc56f59efe4d69c4d849853d92d1a6983a2c21
e-carlin <evan@carlin.com>
2024-01-18 21:15:59 +0000

    fix tests

--- tests/pkcli/build_data/1.out/srv/host/v9.radia.run/vm_devbox_user_1.sh
+++ tests/pkcli/build_data/1.out/srv/host/v9.radia.run/vm_devbox_user_1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 vm_devbox_user_1_rsconf_component() {
-rsconf_service_prepare 'vm_devbox_user_1' '/etc/systemd/system/vm_devbox_user_1.service' '/etc/systemd/system/vm_devbox_user_1.service.d'
+rsconf_service_prepare 'vm_devbox_user_1' '/etc/systemd/system/vm_devbox_user_1.service' '/etc/systemd/system/vm_devbox_user_1.service.d' '/srv/vm_devbox_user_1/start'
 rsconf_install_access '500' 'vagrant' 'vagrant'
-rsconf_install_file '/srv/vm_devbox_user_1/start' '2efa12abc10e3d7c17dd438a20f7e246'
+rsconf_install_file '/srv/vm_devbox_user_1/start' '98bf9be0ac59d7d6f5764bd8eecfc775'
 rsconf_install_access '444' 'root' 'root'
-rsconf_install_file '/etc/systemd/system/vm_devbox_user_1.service' '71ffebabf4f912d63c38d4ca357ab79a'
+rsconf_install_file '/etc/systemd/system/vm_devbox_user_1.service' '3c65a04296d9a4798a9d48c4ddeb322c'
 }
