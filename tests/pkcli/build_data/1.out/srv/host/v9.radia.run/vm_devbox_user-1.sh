109a1993a373da2e0f434727f941bc0096b49b29
e-carlin <evan@carlin.com>
2024-02-05 21:07:03 +0000

    Fix #440: various fixes for vm_devbox systemd
    - remove PrivateTmp this was causing many problems
    - Type=forking the vm process forks and stays running after vagrant up exits
    - vagrant reload after setting forwarded_port (need to reload for change to take)
    - execStop needs to be an abspath and needs to be in /srv/vm_devbox_user. Just use a stop script and call it
    - TimeoutStartSec=10min. With Type=forking the timeout is at play

--- tests/pkcli/build_data/1.out/srv/host/v9.radia.run/vm_devbox_user-1.sh
+++ tests/pkcli/build_data/1.out/srv/host/v9.radia.run/vm_devbox_user-1.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 vm_devbox_user-1_rsconf_component() {
-rsconf_service_prepare 'vm_devbox_user-1' '/etc/systemd/system/vm_devbox_user-1.service' '/etc/systemd/system/vm_devbox_user-1.service.d' '/srv/vm_devbox_user-1/start'
+rsconf_service_prepare 'vm_devbox_user-1' '/etc/systemd/system/vm_devbox_user-1.service' '/etc/systemd/system/vm_devbox_user-1.service.d' '/srv/vm_devbox_user-1/start' '/srv/vm_devbox_user-1/stop'
 rsconf_install_access '700' 'vagrant' 'vagrant'
 rsconf_install_directory '/srv/vm_devbox_user-1'
 rsconf_install_access '500' 'vagrant' 'vagrant'
-rsconf_install_file '/srv/vm_devbox_user-1/start' 'b7d61a2d0f37dc1abdf8a6a799ac25fb'
+rsconf_install_file '/srv/vm_devbox_user-1/start' '5dc3180f1dfd32e5d9d15e426928e94a'
+rsconf_install_file '/srv/vm_devbox_user-1/stop' '5dc3180f1dfd32e5d9d15e426928e94a'
 rsconf_install_access '444' 'root' 'root'
-rsconf_install_file '/etc/systemd/system/vm_devbox_user-1.service' '1bd0318206a0d747ce7ab0247d1d6b39'
+rsconf_install_file '/etc/systemd/system/vm_devbox_user-1.service' 'bcc048be3260244008a695125c53fc9f'
 }
