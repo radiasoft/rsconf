6e7ccf508f9d9f58f6fb88247a26e594eb38df4a
Evan Carlin <evan@carlin.com>
2024-02-06 15:57:48 -0700

    Fix #450: fix tests (#451)

--- tests/pkcli/build_data/1.out/srv/host/v9.radia.run/vm_devbox_user-1.sh
+++ tests/pkcli/build_data/1.out/srv/host/v9.radia.run/vm_devbox_user-1.sh
@@ -7,5 +7,5 @@ rsconf_install_access '500' 'vagrant' 'vagrant'
 rsconf_install_file '/srv/vm_devbox_user-1/start' '081baa61746f4dd28339816bb90453ed'
 rsconf_install_file '/srv/vm_devbox_user-1/stop' '94f5e7855deadc753f54580dfad70217'
 rsconf_install_access '444' 'root' 'root'
-rsconf_install_file '/etc/systemd/system/vm_devbox_user-1.service' '9bea6d3fc6b169474e19863d1e2da5a3'
+rsconf_install_file '/etc/systemd/system/vm_devbox_user-1.service' '95beabb09a3e8a9977002d11ae4e47cd'
 }
