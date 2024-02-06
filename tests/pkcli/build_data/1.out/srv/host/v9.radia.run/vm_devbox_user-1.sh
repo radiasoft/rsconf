1e9948632e8dfa6311e8b4c93f35a066e7623c7f
e-carlin <evan@carlin.com>
2024-02-06 16:57:17 +0000

    Fix #440: configuration to make ssh more secure

--- tests/pkcli/build_data/1.out/srv/host/v9.radia.run/vm_devbox_user-1.sh
+++ tests/pkcli/build_data/1.out/srv/host/v9.radia.run/vm_devbox_user-1.sh
@@ -4,7 +4,7 @@ rsconf_service_prepare 'vm_devbox_user-1' '/etc/systemd/system/vm_devbox_user-1.
 rsconf_install_access '700' 'vagrant' 'vagrant'
 rsconf_install_directory '/srv/vm_devbox_user-1'
 rsconf_install_access '500' 'vagrant' 'vagrant'
-rsconf_install_file '/srv/vm_devbox_user-1/start' '03f7809d5a7ef88b6a4ce734814a8ec8'
+rsconf_install_file '/srv/vm_devbox_user-1/start' '081baa61746f4dd28339816bb90453ed'
 rsconf_install_file '/srv/vm_devbox_user-1/stop' '94f5e7855deadc753f54580dfad70217'
 rsconf_install_access '444' 'root' 'root'
 rsconf_install_file '/etc/systemd/system/vm_devbox_user-1.service' '9bea6d3fc6b169474e19863d1e2da5a3'
