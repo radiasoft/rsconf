d1ca1667763bc224bbbd30f5306f742dd27677ab
robnagler <github@q33.us>
2024-03-27 23:30:35 +0000

    build_test passes

--- rsconf/component/opendkim.py
+++ rsconf/component/opendkim.py
@@ -1,4 +1,4 @@
-"""create postgrey configuration
+"""create dkim configuration for named and/or opendkim
 
 :copyright: Copyright (c) 2017 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
@@ -6,8 +6,8 @@
 
 from pykern.pkcollections import PKDict
 from pykern.pkdebug import pkdc, pkdlog, pkdp
-from rsconf import component
 from pykern import pkio
+from rsconf import component
 
 _CONF_D = pkio.py_path("/etc/postfix")
 _CONF_F = pkio.py_path("/etc/opendkim.conf")
@@ -17,24 +17,20 @@ _SECRET_SUBDIR = "opendkim"
 
 class T(component.T):
     def internal_build_compile(self):
-        from rsconf import systemd
-        from rsconf.component import network
-
         jc, z = self.j2_ctx_init()
-        if z.get("named_only"):
-            self._named_compile(jc, z)
+        if self._named_compile(jc, z):
             return
+        z.pksetdefault(port=8891)
         self.append_root_bash("rsconf_yum_install opendkim")
         z.update(
             external_ignore_list_f=_CONF_D.join("ExternalIgnoreList"),
             internal_hosts_f=_CONF_D.join("InternalHosts"),
             key_table_f=_CONF_D.join("KeyTable"),
             keys_d=_KEYS_D,
-            keys=PKDict(),
-            port=8891,
             run_u="opendkim",
             signing_table_f=_CONF_D.join("SigningTable"),
         )
+        self._read_keys(jc, z)
         self.buildt.require_component("postfix").setup_opendkim(self)
 
     def internal_build_write(self):
@@ -43,45 +39,60 @@ class T(component.T):
 
         jc = self.j2_ctx
         z = jc[self.name]
-        if z.get("named_only"):
-            self._named_write(jc, z)
+        if self._named_write(jc, z):
             return
         systemd.unit_prepare(self, jc, watch_files=(_CONF_D, _CONF_F))
         self.install_access(mode="400", owner=z.run_u)
-        self._install_keys(z.domains)
+        self._install_keys(jc, z)
         systemd.unit_enable(self, jc)
         access("400", run_u)
 
-    def _gen_keys(self, domains, named=False):
+    def _read_keys(self, jc, z):
+        from rsconf import db
+        from rsconf.pkcli import opendkim
+
         def _find(secret_d):
             rv = PKDict()
-            for d in domains:
-                rv[d] = PKDict(keys=[], secret_d=secret_d.join(d))
+            for d in z.domains:
+                rv[d] = PKDict(rows=[], secret_d=secret_d.join(d))
                 for p in pkio.sorted_glob(rv[d].secret_d.join("*.private")):
-                    rv[d].append(
+                    rv[d].rows.append(
                         PKDict(
                             private_f=p, txt_f=p.new(ext="txt"), selector=p.purebasename
                         )
                     )
             return rv
 
-        rv = _find(db.secret_path(j2_ctx, SECRET_SUBDIR, visibility="global"))
-        for d, v in rv.items():
-            if not v.keys:
-                if named:
-                    v.keys.append(rsconf.pkcli.opendkim.gen_key(v.secret_d, d))
-                else:
+        z._keys = _find(db.secret_path(jc, _SECRET_SUBDIR, visibility="global"))
+        for d, v in z._keys.items():
+            if not v.rows:
+                if not z.named_conf_d:
                     raise AssertionError(f"missing keys for domain={d}")
-            for k in v.keys:
-                k.dns = rsconf.pkcli.opendkim.parse_txt(k.txt_f)
-        return rv
+                v.rows.append(opendkim.gen_key(v.secret_d, d))
+            for k in v.rows:
+                k.dns = opendkim.parse_txt(k.txt_f)
 
     def _named_write(self, jc, z):
-        pass
+        def _content():
+            rv = ""
+            for d in sorted(z._keys.keys()):
+                rv += f"    '{d}' => [\n"
+                for k in sorted(z._keys[d].rows, key=lambda x: x.dns.subdomain):
+                    rv += f"        ['{k.dns.subdomain}' => '{k.dns.txt}'],\n"
+                rv += "    ],\n"
+            return rv
+
+        if not z.named_conf_d:
+            return False
+        z.named_content = _content()
+        self.install_resource2("opendkim-named.pl", z.named_conf_d)
+        return z.named_only
 
     def _named_compile(self, jc, z):
-        jc = self.j2_ctx
-        z = jc[self.name]
-
-        self.append_root_bash("rsconf_yum_install opendkim")
-        # NamedConf.zones.domain.txt = [[key, txt]]
+        z.pksetdefault(named_conf_d=None)
+        if not z.named_conf_d:
+            return False
+        z.pksetdefault(named_only=True)
+        self._read_keys(jc, z)
+        self.append_root_bash("rsconf_yum_install opendkim-tools")
+        return z.named_only
