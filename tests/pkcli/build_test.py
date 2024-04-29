74b12daf99e7ab267ce5ca157002a8094c68088a
robnagler <github@q33.us>
2024-04-29 21:32:37 +0000

    fix bad merge

--- tests/pkcli/build_test.py
+++ tests/pkcli/build_test.py
@@ -1,27 +1,26 @@
-# -*- coding: utf-8 -*-
 """test rsconf build
 
 :copyright: Copyright (c) 2021 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
-import pytest
 
-
-def test_cases():
+def test_build():
     from pykern import pkconfig, pkunit, pkio
     from pykern.pkdebug import pkdlog
     from pykern.pkcollections import PKDict
     from rsconf.pkcli import build
     from rsconf import db
 
-    pkdlog(
-        """
+    try:
+        for d in pkunit.case_dirs():
+            pkio.mkdir_parent(d.join("srv"))
+            build.default_command()
+    except Exception:
+        pkdlog(
+            """**RSCONF DEVELOPERS**
 If you get an error caused by the hash in a component.sh file,
-update the component.sh file to see what changed in the file to be downloaded.
-    """
-    )
-    for d in pkunit.case_dirs():
-        pkio.mkdir_parent(d.join("srv"))
+update the component.sh file to see what changed in the file to be downloaded."""
+        )
         raise
 
 
