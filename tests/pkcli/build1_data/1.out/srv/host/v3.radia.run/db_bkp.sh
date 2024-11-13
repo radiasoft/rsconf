c35dfae7c5d01af23659fdd94f63254e50a037ef
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2024-11-12 17:15:30 -0700

    Fix #525 timer_unit_service.jinja remove install target (#526)

--- tests/pkcli/build1_data/1.out/srv/host/v3.radia.run/db_bkp.sh
+++ tests/pkcli/build1_data/1.out/srv/host/v3.radia.run/db_bkp.sh
@@ -7,7 +7,7 @@ rsconf_install_access '500' 'root' 'root'
 rsconf_install_file '/srv/db_bkp/start' 'ede6d1b77f171c00358e47296b67c19f'
 rsconf_install_access '444' 'root' 'root'
 rsconf_install_file '/etc/systemd/system/db_bkp.timer' '64cdcbfab454f653ae9eb19bb0e138a3'
-rsconf_install_file '/etc/systemd/system/db_bkp.service' 'e122d3a763a4419ecd7449f4be5f2b0d'
+rsconf_install_file '/etc/systemd/system/db_bkp.service' '3fae29a217fc19782485d59cb3d5a732'
 rsconf_install_access '500' 'root' 'root'
 rsconf_install_file '/srv/db_bkp/run' '07b2df16d25e42b467105336f512f8f0'
 }
