ef8052e771624e2d8b9b850bcaa9e3cf1e7c70eb
Evan Carlin <evan@carlin.com>
2025-02-13 16:13:54 -0700

    Fix #542: Switch to libvirt for vm_devbox (#543)
    Companion PR for: https://github.com/radiasoft/download/pull/739

--- tests/pkcli/build1_data/1.out/srv/host/v9.radia.run/vm_devbox.sh
+++ tests/pkcli/build1_data/1.out/srv/host/v9.radia.run/vm_devbox.sh
@@ -5,20 +5,15 @@ vm_devbox_main
 #!/bin/bash
 
 vm_devbox_main() {
-    if vagrant --version > /dev/null 2>&1; then
+    if rpm -q libvirt-devel &> /dev/null; then
         return
     fi
-    declare p=kernel-devel-$(uname -r)
-    if ! yum list "$p" &> /dev/null; then
-       install_err "rpm $p not found.
-Virtualbox needs the kernel-devel rpm for the host kernel to be installed.
-Maybe try updating the kernel? The repos only have kernel-devel for recent versions of the kernel."
-    fi
-    rsconf_yum_install "$p"
-    yum-config-manager --add-repo https://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo
-    yum makecache -y
-    rsconf_yum_install VirtualBox-7.0
+    yum-config-manager --set-enabled crb
+    rsconf_yum_install '@Virtualization Hypervisor' '@Virtualization Tools' '@Development Tools' libvirt-devel
     yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
     rsconf_yum_install vagrant
+    systemctl enable --now libvirtd
+    systemctl start libvirtd
+    usermod --append --groups libvirt 'vagrant'
 }
 
