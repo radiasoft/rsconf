d698806813b1966d64f29907863a22beaed9c6ca
Evan Carlin <evan@carlin.com>
2025-04-09 09:41:11 -0600

    Fix #567: Get NFS working on Almalinux (#575)
    - Configuration is now in /etc/nfs.conf.d/
    - Conf file structure has changed

--- rsconf/component/nfs_server.py
+++ rsconf/component/nfs_server.py
@@ -4,8 +4,7 @@
 :copyright: Copyright (c) 2018 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
-from __future__ import absolute_import, division, print_function
-from pykern import pkcollections
+from pykern.pkcollections import PKDict
 from pykern import pkio
 from pykern.pkdebug import pkdp
 from rsconf import component
@@ -16,12 +15,20 @@ _OPTIONS = "rw,no_root_squash,no_subtree_check,async,secure"
 
 
 class T(component.T):
-    def internal_build(self):
+    def internal_build_compile(self):
         self.buildt.require_component("base_all")
 
         # Must be first to ensure /etc/exports.d exists
-        j2_ctx = self.hdb.j2_ctx_copy()
-        z = j2_ctx.nfs_server
+        jc = self.j2_ctx = self.hdb.j2_ctx_copy()
+        z = jc.nfs_server
+        if self.hdb.rsconf_db.is_centos7:
+            z.conf_f = pkio.py_path("/etc/sysconfig/nfs")
+        else:
+            z.conf_f = pkio.py_path("/etc/nfs.conf.d/99-rsconf.conf")
+
+    def internal_build_write(self):
+        jc = self.j2_ctx
+        z = jc.nfs_server
         self.append_root_bash("rsconf_yum_install nfs-utils")
         # Don't use "nfs", because "systemctl is-active nfs" returns unknown, b/c
         # nfs is a symlink to nfs-server.
@@ -32,11 +39,20 @@ class T(component.T):
             z.fs = fs
             # /foo/bar => foo_bar.exports
             f = fs[1:].replace("/", "_") + ".exports"
-            self.install_resource("nfs_server/exports", j2_ctx, _EXPORTS_D.join(f))
-        x = "RPCNFSDCOUNT={}".format(z.num_servers)
-        self.rsconf_edit(
-            _SYSCONFIG_NFS,
-            "^{}$".format(x),
-            r"s<^#?\s*RPCNFSDCOUNT.*><{}>".format(x),
-        )
-        self.rsconf_service_restart()
+            self.install_resource("nfs_server/exports", jc, _EXPORTS_D.join(f))
+        if self.hdb.rsconf_db.is_centos7:
+            x = "RPCNFSDCOUNT={}".format(z.num_servers)
+            self.rsconf_edit(
+                z.conf_f,
+                "^{}$".format(x),
+                r"s<^#?\s*RPCNFSDCOUNT.*><{}>".format(x),
+            )
+            self.rsconf_service_restart()
+        else:
+            self.install_resource2(
+                "nfs_server.conf",
+                z.conf_f.dirpath(),
+                host_f=z.conf_f.basename,
+                access="400",
+                host_d_access=PKDict(mode="700", owner=self.hdb.rsconf_db.root_u),
+            )
