37bb0fe9feacbe02582276a8ae27226d319c05fd
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2026-01-01 16:28:04 -0600

    Fix #605 nfs_client do not pin version; noacl is obsolete (#606)

--- tests/pkcli/build1_data/1.out/srv/host/v9.radia.run/nfs_client.sh
+++ tests/pkcli/build1_data/1.out/srv/host/v9.radia.run/nfs_client.sh
@@ -15,7 +15,7 @@ nfs_client_add() {
     # https://wiki.archlinux.org/index.php/NFS#Mount_using_.2Fetc.2Ffstab
     # May want to add: noatime,nodiratime; We aren't using ACLs
     # relatime is there by default on RH. rsize and wsize are large by default, too
-    local options=defaults,vers=4.1,soft,noacl,_netdev
+    local options=defaults,soft,_netdev
     rsconf_install_access 700 root root
     rsconf_install_mount_point "$mount"
     # Technically can mount $fs twice so what we want to ensure is $mount is not twice
