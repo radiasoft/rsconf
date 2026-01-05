367ecdd877788464106cc9a7fe6323dbcd900ccb
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2026-01-05 16:59:37 +0000

    fmt

--- rsconf/component/base_os.py
+++ rsconf/component/base_os.py
@@ -3,6 +3,7 @@
 :copyright: Copyright (c) 2017 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
+
 from pykern.pkcollections import PKDict
 from pykern import pkio
 from pykern.pkdebug import pkdp
@@ -10,7 +11,7 @@ from rsconf import component
 
 _JOURNAL_CONF_D = pkio.py_path("/etc/systemd/journald.conf.d")
 _SSHD_CONF_D = pkio.py_path("/etc/ssh")
-_JOURNAL_SYSTEMD_D=pkio.py_path("/etc/systemd/system/systemd-journald.service.d")
+_JOURNAL_SYSTEMD_D = pkio.py_path("/etc/systemd/system/systemd-journald.service.d")
 
 
 class T(component.T):
@@ -65,7 +66,12 @@ class T(component.T):
         self.install_resource2("hostname", "/etc", access="444")
         self.install_resource2("motd", "/etc")
         if self.hdb.rsconf_db.is_almalinux9:
-            self.install_resource2("99-journald-rsconf.conf", _JOURNAL_SYSTEMD_D, access="444", host_d_access="755")
+            self.install_resource2(
+                "99-journald-rsconf.conf",
+                _JOURNAL_SYSTEMD_D,
+                access="444",
+                host_d_access="755",
+            )
         self._pam_duo_and_sshd()
         self.append_root_bash_with_main(jc)
 
