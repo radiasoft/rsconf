02f53283cdad71b0c672867fc75dabc956366140
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2026-04-06 22:47:35 +0000

    2 working and named-checkconf

--- tests/named_conf_test.py
+++ tests/named_conf_test.py
@@ -4,9 +4,20 @@
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
 
-def test_build(monkeypatch):
-    from pykern import pkconfig, pkunit, pkio
+
+def test_build():
+    from pykern import pkunit, pkio
     from rsconf import named_conf
+    import shutil, subprocess
 
     for d in pkunit.case_dirs():
-        named_conf.generate("/srv/named/db", d.join('in.py'), [], test_serial="2023111502")
+        named_conf.generate(
+            str(d),
+            d.join("in.py"),
+            pkio.sorted_glob("*.json"),
+            test_serial="2023111502",
+        )
+        if x := shutil.which("named-checkconf"):
+            subprocess.check_call([x, "-z", "named.conf"])
+        f = d.join("named.conf")
+        pkio.write_text(f, pkio.read_text(f).replace(str(d), "/srv/named/db"))
